4189f09ecbceb71f74cb3a5afe326310133c8eee
Dan Freeman <dfreeman@salsify.com>
2022-07-19 17:01:41 +0200

    Run no-implicit-this codemod

--- tests/integration/components/ember-ace-test.js
+++ tests/integration/components/ember-ace-test.js
@@ -22,7 +22,7 @@ module('Integration | Component | ember ace', function(hooks) {
 
   test('rendering a given value', async function(assert) {
     this.set('value', 'function() {\n  console.log("hi");\n}');
-    await render(hbs`{{ember-ace lines=3 value=value}}`);
+    await render(hbs`{{ember-ace lines=3 value=this.value}}`);
 
     assert.equal(this.component.lines.length, 3);
     assert.equal(this.component.value, this.get('value'));
@@ -34,7 +34,7 @@ module('Integration | Component | ember ace', function(hooks) {
 
   test('leading, trailing and internal whitespace', async function(assert) {
     this.set('value', '\n\na\n  \nb\n\n');
-    await render(hbs`{{ember-ace lines=10 value=value}}`);
+    await render(hbs`{{ember-ace lines=10 value=this.value}}`);
 
     assert.equal(this.component.lines.length, 7);
     assert.equal(this.component.value, this.get('value'));
@@ -42,7 +42,7 @@ module('Integration | Component | ember ace', function(hooks) {
 
   test('internal value updates', async function(assert) {
     this.set('change', sinon.spy());
-    await render(hbs`{{ember-ace lines=1 update=(action change)}}`);
+    await render(hbs`{{ember-ace lines=1 update=(action this.change)}}`);
 
     this.component.setValue('hello');
     assert.ok(this.get('change').calledWith('hello'));
@@ -53,7 +53,7 @@ module('Integration | Component | ember ace', function(hooks) {
   test('internal value updates with initial value', async function(assert) {
     this.set('value', 'one');
     this.set('change', sinon.spy());
-    await render(hbs`{{ember-ace lines=1 update=(action change)}}`);
+    await render(hbs`{{ember-ace lines=1 update=(action this.change)}}`);
 
     this.component.setValue('two');
     assert.equal(this.get('change.callCount'), 1);
@@ -67,7 +67,7 @@ module('Integration | Component | ember ace', function(hooks) {
   test('external value updates', async function(assert) {
     this.set('value', 'one');
     this.set('change', sinon.spy());
-    await render(hbs`{{ember-ace lines=1 value=value update=(action change)}}`);
+    await render(hbs`{{ember-ace lines=1 value=this.value update=(action this.change)}}`);
 
     run(() => this.set('value', 'two'));
     assert.equal(this.get('change.callCount'), 0);
@@ -76,7 +76,7 @@ module('Integration | Component | ember ace', function(hooks) {
 
   test('setting a theme', async function(assert) {
     this.set('theme', 'ace/theme/ambiance');
-    await render(hbs`{{ember-ace theme=theme}}`);
+    await render(hbs`{{ember-ace theme=this.theme}}`);
     assert.ok(this.$('.ace_editor').is('.ace-ambiance'));
 
     run(() => this.set('theme', 'ace/theme/chaos'));
@@ -86,7 +86,7 @@ module('Integration | Component | ember ace', function(hooks) {
   test('annotating lines', async function(assert) {
     this.set('annotations', [{ type: 'warning', row: 1 }]);
     this.set('value', 'hello\nworld');
-    await render(hbs`{{ember-ace lines=3 annotations=annotations value=value}}`);
+    await render(hbs`{{ember-ace lines=3 annotations=this.annotations value=this.value}}`);
     assert.equal(this.component.annotations.length, 1);
     assert.equal(this.component.annotations.objectAt(0).type, 'warning');
     assert.equal(this.component.annotations.objectAt(0).row, 1);
@@ -106,7 +106,7 @@ module('Integration | Component | ember ace', function(hooks) {
       { class: 'foo', range: new Range(0, 0, 1, 1) },
       { class: 'bar', range: new Range(2, 0, 2, 5) }
     ]);
-    await render(hbs`{{ember-ace lines=3 markers=markers value=value}}`);
+    await render(hbs`{{ember-ace lines=3 markers=this.markers value=this.value}}`);
     assert.equal(this.component.backMarkers.length, 0);
     assert.equal(this.component.frontMarkers.length, 2);
     assert.equal(this.component.frontMarkers.objectAt(0).type, 'foo');
@@ -129,7 +129,7 @@ module('Integration | Component | ember ace', function(hooks) {
       { class: 'foo', range: new Range(0, 0, 1, 1), inFront: false },
       { class: 'bar', range: new Range(2, 0, 2, 5), inFront: false }
     ]);
-    await render(hbs`{{ember-ace lines=3 markers=markers value=value}}`);
+    await render(hbs`{{ember-ace lines=3 markers=this.markers value=this.value}}`);
     assert.equal(this.component.frontMarkers.length, 0);
     assert.equal(this.component.backMarkers.length, 2);
     assert.equal(this.component.backMarkers.objectAt(0).type, 'foo');
@@ -153,7 +153,7 @@ module('Integration | Component | ember ace', function(hooks) {
       { type: 'info', range: new Range(2, 0, 2, 5), text: 'btw' }
     ]);
 
-    await render(hbs`{{ember-ace lines=3 overlays=overlays value=value}}`);
+    await render(hbs`{{ember-ace lines=3 overlays=this.overlays value=this.value}}`);
     assert.equal(this.component.annotations.length, 2);
     assert.equal(this.component.frontMarkers.length, 2);
     assert.equal(this.component.backMarkers.length, 0);
@@ -171,7 +171,7 @@ module('Integration | Component | ember ace', function(hooks) {
 
   test('default autocomplete', async function(assert) {
     this.set('value', 'foo\nbar\nf');
-    await render(hbs`{{ember-ace lines=3 value=value enableDefaultAutocompletion=true}}`);
+    await render(hbs`{{ember-ace lines=3 value=this.value enableDefaultAutocompletion=true}}`);
 
     const { autocomplete } = this.component;
 
@@ -192,7 +192,7 @@ module('Integration | Component | ember ace', function(hooks) {
       ];
     });
 
-    await render(hbs`{{ember-ace lines=3 value='text' suggestCompletions=(action suggestCompletions)}}`);
+    await render(hbs`{{ember-ace lines=3 value='text' suggestCompletions=(action this.suggestCompletions)}}`);
     const { autocomplete } = this.component;
 
     await autocomplete.trigger();
@@ -220,7 +220,7 @@ module('Integration | Component | ember ace', function(hooks) {
     });
 
     await render(hbs`
-      {{#ember-ace lines=3 value='text' suggestCompletions=(action suggestCompletions) as |editor|}}
+      {{#ember-ace lines=3 value='text' suggestCompletions=(action this.suggestCompletions) as |editor|}}
         {{#editor.completion-tooltip as |suggestion|}}
           Payload: {{suggestion.extra}}
         {{/editor.completion-tooltip}}
@@ -247,7 +247,7 @@ module('Integration | Component | ember ace', function(hooks) {
 
     this.set('mode', new NumberMode());
     this.set('value', 'abc 123\n!@# 456 foo');
-    await render(hbs`{{ember-ace mode=mode value=value}}`);
+    await render(hbs`{{ember-ace mode=this.mode value=this.value}}`);
     assert.deepEqual(this.component.lines.objectAt(0).tokens.mapBy('type'), ['other', 'constant.numeric']);
     assert.deepEqual(this.component.lines.objectAt(1).tokens.mapBy('type'), ['other', 'constant.numeric', 'other']);
 
